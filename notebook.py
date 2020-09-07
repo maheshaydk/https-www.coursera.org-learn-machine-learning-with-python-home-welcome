5abcf03d0be2d6efeaa0a2d0ce91e59111e2587c
maheshaydk <maheshaydk@gmail.com>
2020-09-07 18:21:53 +0530

    jupyter notebook
    this notebook will be mainly used for the capstone project.

---
+++ notebook.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#this notebook isonly for capstone project
+
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
